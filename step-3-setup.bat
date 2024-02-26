6ff054a07c6fb2614a4215087108f2e18979e7e9
Fujii Hironori <fujii.hironori@gmail.com>
2024-02-26 09:53:54 +0900

    reg add and netsh

--- step-3-setup.bat
+++ step-3-setup.bat
@@ -4,3 +4,9 @@
 
 python -m pip install pywin32
 call gem install webrick
+
+reg add HKEY_CLASSES_ROOT\.pl\Shell\ExecCGI\Command /f /ve /d "c:\xampp\perl\bin\perl.exe -T"
+reg add HKEY_CLASSES_ROOT\.cgi\Shell\ExecCGI\Command /f /ve /d "c:\xampp\perl\bin\perl.exe -T"
+reg add HKEY_CLASSES_ROOT\.py\Shell\ExecCGI\Command /f /ve /d "c:\Python311\python.exe -X utf8"
+
+netsh int ipv4 set dynamicport tcp start=1025 num=64511
