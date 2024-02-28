f228a0f999c3b8a2074d90890cfb194fcad15c9e
Fujii Hironori <fujii.hironori@gmail.com>
2024-02-28 16:13:39 +0900

    Add /GitAndUnixToolsOnPath
    WinCairo EWS is using unix tools.

--- step-2-choco-install.bat
+++ step-2-choco-install.bat
@@ -1,5 +1,5 @@
 choco install -y xampp-81 python311 ruby gperf llvm ninja
 choco install -y visualstudio2022buildtools visualstudio2022-workload-vctools
 choco install -y cmake.install --installargs 'ADD_CMAKE_TO_PATH=System'
-choco install -y git.install --params="/NoAutoCrlf /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration"
+choco install -y git.install --params="/NoAutoCrlf /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration /GitAndUnixToolsOnPath"
 if errorlevel 1 pause
