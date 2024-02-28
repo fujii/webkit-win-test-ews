a54631b461060e329d13217faa70ec50432fee92
Fujii Hironori <fujii.hironori@gmail.com>
2024-02-29 06:41:34 +0900

    Install diffutils

--- step-2-choco-install.bat
+++ step-2-choco-install.bat
@@ -1,4 +1,4 @@
-choco install -y xampp-81 python311 ruby gperf llvm ninja
+choco install -y xampp-81 python311 ruby gperf llvm ninja diffutils
 choco install -y visualstudio2022buildtools visualstudio2022-workload-vctools
 choco install -y cmake.install --installargs 'ADD_CMAKE_TO_PATH=System'
 choco install -y git.install --params="/NoAutoCrlf /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration /GitAndUnixToolsOnPath"
