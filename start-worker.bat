76e88e087325bc5d762062fa6486c8b4a7dc241b
Fujii Hironori <fujii.hironori@gmail.com>
2024-02-29 11:29:13 +0900

    Set WEBKIT_LIBRARIES

--- start-worker.bat
+++ start-worker.bat
@@ -1,6 +1,9 @@
 %~d0
 cd %~p0
 
+set WEBKIT_LIBRARIES=c:\ews\libs
+if not exist %WEBKIT_LIBRARIES% mkdir %WEBKIT_LIBRARIES%
+
 set WEBKIT_TEST_CHILD_PROCESSES=3
 set TEMP=C:\ews\temp
 set TMP=C:\ews\temp
