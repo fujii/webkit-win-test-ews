9916cb561475e50d54dda37a5784dfd98f9711a2
Fujii Hironori <fujii.hironori@gmail.com>
2024-02-29 15:06:42 +0900

    Add /f to reg add to suppess prompt

--- step-4-setup-worker.bat
+++ step-4-setup-worker.bat
@@ -4,7 +4,7 @@ if not exist c:\ews mkdir c:\ews
 if not exist c:\ews\symcache mkdir c:\ews\symcache
 if not exist c:\ews\temp mkdir c:\ews\temp
 
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v _NT_SYMBOL_PATH /d "cache*C:\ews\symcache;srv*https://msdl.microsoft.com/download/symbols"
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v _NT_SYMBOL_PATH /d "cache*C:\ews\symcache;srv*https://msdl.microsoft.com/download/symbols"
 
 cd c:\ews
 if not exist setup-tools git clone https://github.com/fujii/webkit-win-test-ews setup-tools
