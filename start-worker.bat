1446439e4c538d08f07e7e09ce2499ca58ee4ee0
Fujii Hironori <fujii.hironori@gmail.com>
2024-02-29 14:34:00 +0900

    Call vcvars64.bat

--- start-worker.bat
+++ start-worker.bat
@@ -1,5 +1,6 @@
-%~d0
-cd %~p0
+path %ProgramFiles(x86)%\Microsoft Visual Studio\Installer;%path%
+for /F "usebackq delims=" %%I in (`vswhere.exe -products * -latest -property installationPath`) do set VSPATH=%%I
+call "%VSPATH%\VC\Auxiliary\Build\vcvars64.bat"
 
 set WEBKIT_LIBRARIES=c:\ews\libs
 if not exist %WEBKIT_LIBRARIES% mkdir %WEBKIT_LIBRARIES%
@@ -11,4 +12,6 @@ set TMP=C:\ews\temp
 set CC=clang-cl
 set CXX=clang-cl
 
-buildbot-worker start c:\ews
+C:
+cd \ews
+buildbot-worker start
