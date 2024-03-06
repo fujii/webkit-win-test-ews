b449a1439d6d303cb3a7a941e7c428341e4595df
Fujii Hironori <Hironori.Fujii@sony.com>
2024-03-06 12:29:23 +0900

    set hostname

--- step-4-setup-worker.bat
+++ step-4-setup-worker.bat
@@ -1,8 +1,13 @@
 python -m pip install buildbot-worker==3.7.0
 
 if not exist c:\ews mkdir c:\ews
+if not exist c:\ews\info mkdir c:\ews\info
 if not exist c:\ews\symcache mkdir c:\ews\symcache
 if not exist c:\ews\temp mkdir c:\ews\temp
+
+for /F "usebackq delims=" %%I in (`hostname`) do set hostname=%%I
+echo %hostname% provided by Playwright team > c:\ews\info\host
+echo Fujii Hironori > c:\ews\info\admin
 
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v _NT_SYMBOL_PATH /d "cache*C:\ews\symcache;srv*https://msdl.microsoft.com/download/symbols"
 
