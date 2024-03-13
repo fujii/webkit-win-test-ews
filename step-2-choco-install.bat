1b0105b39cb49d978fdd2cb240158c30f1934f30
Fujii Hironori <Hironori.Fujii@sony.com>
2024-03-13 10:43:20 +0900

    Install llvm 17.0.6

--- step-2-choco-install.bat
+++ step-2-choco-install.bat
@@ -1,4 +1,5 @@
-choco install -y xampp-81 python311 ruby gperf llvm ninja diffutils
+choco install -y xampp-81 python311 ruby gperf ninja diffutils
+choco install -y llvm --version=17.0.6
 choco install -y visualstudio2022buildtools visualstudio2022-workload-vctools windows-sdk-10-version-2004-windbg
 choco install -y cmake.install --installargs 'ADD_CMAKE_TO_PATH=System'
 choco install -y git.install --params="/NoAutoCrlf /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration /GitAndUnixToolsOnPath"
