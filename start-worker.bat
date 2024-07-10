922aef7c4c9e7de3b5a351776e1f1021db7c69a5
Fujii Hironori <Hironori.Fujii@sony.com>
2024-07-10 11:13:36 +0900

    WEBKIT_TEST_CHILD_PROCESSES=4

--- start-worker.bat
+++ start-worker.bat
@@ -6,7 +6,7 @@ echo on
 set WEBKIT_LIBRARIES=c:\ews\libs
 if not exist %WEBKIT_LIBRARIES% mkdir %WEBKIT_LIBRARIES%
 
-set WEBKIT_TEST_CHILD_PROCESSES=3
+set WEBKIT_TEST_CHILD_PROCESSES=4
 set TEMP=C:\ews\temp
 set TMP=C:\ews\temp
 
