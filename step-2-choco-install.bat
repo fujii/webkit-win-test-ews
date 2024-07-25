d2086cc8bce5a2a1d1f73b2458a5141b2e4866ee
Fujii Hironori <Hironori.Fujii@sony.com>
2024-07-25 13:24:12 +0900

    Use "choco install" for pywin32 and webrick

--- step-2-choco-install.bat
+++ step-2-choco-install.bat
@@ -3,4 +3,10 @@ choco install -y llvm
 choco install -y visualstudio2022buildtools visualstudio2022-workload-vctools windows-sdk-10-version-2004-windbg
 choco install -y cmake.install --installargs 'ADD_CMAKE_TO_PATH=System'
 choco install -y git.install --params="/NoAutoCrlf /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration /GitAndUnixToolsOnPath"
+
+choco install -y --source=python pywin32
+choco install -y --source=ruby webrick
+
+choco upgrade -y all
+
 if errorlevel 1 pause
--- step-3-setup-tools.bat
+++ step-3-setup-tools.bat
@@ -2,9 +2,6 @@
 %ChocolateyInstall%\tools\shimgen -o %ChocolateyInstall%\bin\openssl.exe -p c:\xampp\apache\bin\openssl.exe
 %ChocolateyInstall%\tools\shimgen -o %ChocolateyInstall%\bin\python3.exe -p c:\Python311\python.exe
 
-python -m pip install pywin32
-call gem install webrick
-
 reg add HKEY_CLASSES_ROOT\.pl\Shell\ExecCGI\Command /f /ve /d "c:\xampp\perl\bin\perl.exe -T"
 reg add HKEY_CLASSES_ROOT\.cgi\Shell\ExecCGI\Command /f /ve /d "c:\xampp\perl\bin\perl.exe -T"
 reg add HKEY_CLASSES_ROOT\.py\Shell\ExecCGI\Command /f /ve /d "c:\Python311\python.exe -X utf8"
