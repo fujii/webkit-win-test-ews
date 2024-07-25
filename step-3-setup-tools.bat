5bc1a0f348b52f59913760eb9eb2ee21d594ef2e
Fujii Hironori <Hironori.Fujii@sony.com>
2024-07-25 13:12:59 +0900

    Fix a comment

--- step-3-setup-tools.bat
+++ step-3-setup-tools.bat
@@ -13,7 +13,7 @@ netsh int ipv4 set dynamicport tcp start=1025 num=64511
 
 git config --system core.longpaths true
 
-# git-credential-manager popups a dialog. It blocks a buildbot worker.
+rem git-credential-manager popups a dialog. It blocks a buildbot worker.
 git config --system --unset credential.helper
 
 C:\xampp\apache\bin\httpd.exe -k install
