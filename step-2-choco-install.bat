262a0cc575424f2cb40f81b5e65851ce3bc7a099
Fujii Hironori <Hironori.Fujii@sony.com>
2024-10-11 12:13:59 +0900

    Bump Python to v3.12

--- step-2-choco-install.bat
+++ step-2-choco-install.bat
@@ -1,4 +1,4 @@
-choco install -y xampp-81 python311 ruby gperf ninja diffutils
+choco install -y xampp-81 python312 ruby gperf ninja diffutils
 choco install -y llvm
 choco install -y visualstudio2022buildtools visualstudio2022-workload-vctools windows-sdk-10-version-2004-windbg
 choco install -y cmake.install --installargs 'ADD_CMAKE_TO_PATH=System'
--- step-3-setup-tools.bat
+++ step-3-setup-tools.bat
@@ -1,13 +1,13 @@
 %ChocolateyInstall%\tools\shimgen -o %ChocolateyInstall%\bin\perl.exe -p c:\xampp\perl\bin\perl.exe
 %ChocolateyInstall%\tools\shimgen -o %ChocolateyInstall%\bin\openssl.exe -p c:\xampp\apache\bin\openssl.exe
-%ChocolateyInstall%\tools\shimgen -o %ChocolateyInstall%\bin\python3.exe -p c:\Python311\python.exe
+%ChocolateyInstall%\tools\shimgen -o %ChocolateyInstall%\bin\python3.exe -p c:\Python312\python.exe
 
 python -m pip install pywin32
 call gem install webrick
 
 reg add HKEY_CLASSES_ROOT\.pl\Shell\ExecCGI\Command /f /ve /d "c:\xampp\perl\bin\perl.exe -T"
 reg add HKEY_CLASSES_ROOT\.cgi\Shell\ExecCGI\Command /f /ve /d "c:\xampp\perl\bin\perl.exe -T"
-reg add HKEY_CLASSES_ROOT\.py\Shell\ExecCGI\Command /f /ve /d "c:\Python311\python.exe -X utf8"
+reg add HKEY_CLASSES_ROOT\.py\Shell\ExecCGI\Command /f /ve /d "c:\Python312\python.exe -X utf8"
 
 netsh int ipv4 set dynamicport tcp start=1025 num=64511
 
