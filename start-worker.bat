7f3fdb0d675fd988348a4ddc490f539f37165514
Fujii Hironori <Hironori.Fujii@sony.com>
2024-11-29 15:22:54 +0900

    start-worker.bat: Get the parent directory of this batch file

--- start-worker.bat
+++ start-worker.bat
@@ -1,14 +1,19 @@
+rem Get the parent directory of this batch file
+%~d0
+cd %~p0..
+set bw_dir=%cd%
+
 path %ProgramFiles(x86)%\Microsoft Visual Studio\Installer;%path%
 for /F "usebackq delims=" %%I in (`vswhere.exe -products * -latest -property installationPath`) do set VSPATH=%%I
 call "%VSPATH%\VC\Auxiliary\Build\vcvars64.bat"
 echo on
 
-set WEBKIT_LIBRARIES=c:\BW\libs
+set WEBKIT_LIBRARIES=%bw_dir%libs
 if not exist %WEBKIT_LIBRARIES% mkdir %WEBKIT_LIBRARIES%
 
 set WEBKIT_TEST_CHILD_PROCESSES=4
-set TEMP=c:\BW\temp
-set TMP=c:\BW\temp
+set TEMP=%bw_dir%temp
+set TMP=%bw_dir%temp
 
 set CC=clang-cl
 set CXX=clang-cl
@@ -16,6 +21,4 @@ set CXX=clang-cl
 rem store "Amazon Root CA1" cert
 curl.exe -I https://aws.amazon.com/
 
-C:
-cd \BW
 buildbot-worker start
