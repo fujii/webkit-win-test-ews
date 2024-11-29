b8144d1d7d9b2060f482bf9823deca4c6885b784
Fujii Hironori <Hironori.Fujii@sony.com>
2024-11-29 15:52:27 +0900

    Removed old batch files

--- step-2-choco-install.bat
+++
@@ -1,9 +0,0 @@
-choco install -y xampp-81 python312 ruby gperf ninja diffutils
-choco install -y llvm
-choco install -y visualstudio2022buildtools visualstudio2022-workload-vctools windows-sdk-10-version-2004-windbg
-choco install -y cmake.install --installargs 'ADD_CMAKE_TO_PATH=System'
-choco install -y git.install --params="/NoAutoCrlf /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration /GitAndUnixToolsOnPath"
-
-choco upgrade -y all
-
-if errorlevel 1 pause
