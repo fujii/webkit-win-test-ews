bf05be4a5cdd5e5fddd2558c83f444496a3f2183
Fujii Hironori <Hironori.Fujii@sony.com>
2025-02-12 06:15:18 +0900

    Added delete-temp-files.bat

---
+++ delete-temp-files.bat
@@ -0,0 +1,35 @@
+
+del /s/q/f c:\BW\temp\*
+del /s/q/f c:\BW\symcache\
+
+del /s/q/f c:\BW\symcache\JavaScriptCore.pdb
+del /s/q/f c:\BW\symcache\WebCore.pdb
+del /s/q/f c:\BW\symcache\WebKit2.pdb
+del /s/q/f c:\BW\symcache\WebKitGPUProcess.pdb
+del /s/q/f c:\BW\symcache\WebKitNetworkProcess.pdb
+del /s/q/f c:\BW\symcache\WebKitTestRunner.pdb
+del /s/q/f c:\BW\symcache\WebKitWebProcess.pdb
+
+del /s/q/f c:\BW\symcache\aom.pdb
+del /s/q/f c:\BW\symcache\avif.pdb
+del /s/q/f c:\BW\symcache\brotli*.pdb
+del /s/q/f c:\BW\symcache\cairo.pdb
+del /s/q/f c:\BW\symcache\crypto*.pdb
+del /s/q/f c:\BW\symcache\harfbuzz*.pdb
+del /s/q/f c:\BW\symcache\icu*.pdb
+del /s/q/f c:\BW\symcache\jpeg*.pdb
+del /s/q/f c:\BW\symcache\jxl*.pdb
+del /s/q/f c:\BW\symcache\libcurl.pdb
+del /s/q/f c:\BW\symcache\libexslt.pdb
+del /s/q/f c:\BW\symcache\libpng*.pdb
+del /s/q/f c:\BW\symcache\libsharpyuv.pdb
+del /s/q/f c:\BW\symcache\libwebp*.pdb
+del /s/q/f c:\BW\symcache\libxml2.pdb
+del /s/q/f c:\BW\symcache\libxslt.pdb
+del /s/q/f c:\BW\symcache\nghttp*.pdb
+del /s/q/f c:\BW\symcache\ngtcp2*.pdb
+del /s/q/f c:\BW\symcache\sqlite3.pdb
+del /s/q/f c:\BW\symcache\ssl*.pdb
+del /s/q/f c:\BW\symcache\tls*.pdb
+del /s/q/f c:\BW\symcache\turbojpeg.pdb
+del /s/q/f c:\BW\symcache\zlib.pdb
