01e8d9e0b805507b19369a405cda226f30c98505
Fujii Hironori <Hironori.Fujii@sony.com>
2025-02-12 09:28:19 +0900

    rd doesn't accept a wildcard (*)

--- delete-temp-files.bat
+++ delete-temp-files.bat
@@ -1,31 +1,33 @@
+set f=
 
-rd /s/q c:\BW\temp\*
-rd /s/q c:\BW\symcache\
+set f=%f% c:\BW\temp\*
 
-rd /s/q c:\BW\symcache\JavaScriptCore.pdb
-rd /s/q c:\BW\symcache\WebCore.pdb
-rd /s/q c:\BW\symcache\WebKit*.pdb
+set f=%f% c:\BW\symcache\JavaScriptCore*
+set f=%f% c:\BW\symcache\WebCore*
+set f=%f% c:\BW\symcache\WebKit*
 
-rd /s/q c:\BW\symcache\aom.pdb
-rd /s/q c:\BW\symcache\avif.pdb
-rd /s/q c:\BW\symcache\brotli*.pdb
-rd /s/q c:\BW\symcache\cairo.pdb
-rd /s/q c:\BW\symcache\crypto*.pdb
-rd /s/q c:\BW\symcache\harfbuzz*.pdb
-rd /s/q c:\BW\symcache\icu*.pdb
-rd /s/q c:\BW\symcache\jpeg*.pdb
-rd /s/q c:\BW\symcache\jxl*.pdb
-rd /s/q c:\BW\symcache\libcurl.pdb
-rd /s/q c:\BW\symcache\libexslt.pdb
-rd /s/q c:\BW\symcache\libpng*.pdb
-rd /s/q c:\BW\symcache\libsharpyuv.pdb
-rd /s/q c:\BW\symcache\libwebp*.pdb
-rd /s/q c:\BW\symcache\libxml2.pdb
-rd /s/q c:\BW\symcache\libxslt.pdb
-rd /s/q c:\BW\symcache\nghttp*.pdb
-rd /s/q c:\BW\symcache\ngtcp2*.pdb
-rd /s/q c:\BW\symcache\sqlite3.pdb
-rd /s/q c:\BW\symcache\ssl*.pdb
-rd /s/q c:\BW\symcache\tls*.pdb
-rd /s/q c:\BW\symcache\turbojpeg.pdb
-rd /s/q c:\BW\symcache\zlib.pdb
+set f=%f% c:\BW\symcache\aom*
+set f=%f% c:\BW\symcache\avif*
+set f=%f% c:\BW\symcache\brotli*
+set f=%f% c:\BW\symcache\cairo*
+set f=%f% c:\BW\symcache\crypto*
+set f=%f% c:\BW\symcache\harfbuzz*
+set f=%f% c:\BW\symcache\icu*
+set f=%f% c:\BW\symcache\jpeg*
+set f=%f% c:\BW\symcache\jxl*
+set f=%f% c:\BW\symcache\libcurl*
+set f=%f% c:\BW\symcache\libexslt*
+set f=%f% c:\BW\symcache\libpng*
+set f=%f% c:\BW\symcache\libsharpyuv*
+set f=%f% c:\BW\symcache\libwebp*
+set f=%f% c:\BW\symcache\libxml2*
+set f=%f% c:\BW\symcache\libxslt*
+set f=%f% c:\BW\symcache\nghttp*
+set f=%f% c:\BW\symcache\ngtcp2*
+set f=%f% c:\BW\symcache\sqlite3*
+set f=%f% c:\BW\symcache\ssl*
+set f=%f% c:\BW\symcache\tls*
+set f=%f% c:\BW\symcache\turbojpeg*
+set f=%f% c:\BW\symcache\zlib*
+
+for /d %%i in (%f%) do rd /s/q %%i
