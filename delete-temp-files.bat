bc4f20b6cb75f13857b83e6bb7f62a4e33cb60d5
Fujii Hironori <Hironori.Fujii@sony.com>
2025-02-20 09:56:33 +0900

    Delete *.tmp in %windir%

--- delete-temp-files.bat
+++ delete-temp-files.bat
@@ -5,3 +5,5 @@ mkdir c:\BW\symcache
 cd c:\BW\symcache
 set f=JavaScriptCore* WebCore* WebKit* aom* avif* brotli* cairo* crypto* harfbuzz* icu* jpeg* jxl* libcurl* libexslt* libpng* libsharpyuv* libwebp* libxml2* libxslt* nghttp* ngtcp2* sqlite3* ssl* tls* turbojpeg* zlib*
 for /d %%i in (%f%) do rd /s/q %%i
+
+del /q %windir%\*.tmp
