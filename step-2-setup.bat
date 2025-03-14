ea88529120aa24a8ec5c66d0d4ff1c03bf91ed3e
Fujii Hironori <Hironori.Fujii@sony.com>
2025-03-14 14:52:50 +0900

    Fixed LLVM version to 19.1.7 temporarily
    Bug 289669 – [MagicEnum C++] EnumTraits.h:267:9: error: no matching function for call to 'enumName' with clang-20
    https://bugs.webkit.org/show_bug.cgi?id=289669

--- step-2-setup.bat
+++ step-2-setup.bat
@@ -1,5 +1,5 @@
 choco install -y xampp-81 python312 ruby gperf ninja diffutils
-choco install -y llvm
+choco install -y llvm --version 19.1.7
 choco install -y visualstudio2022buildtools visualstudio2022-workload-vctools windows-sdk-10-version-2004-windbg
 choco install -y cmake.install --installargs 'ADD_CMAKE_TO_PATH=System'
 choco install -y git.install --params="/NoAutoCrlf /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration /GitAndUnixToolsOnPath"
